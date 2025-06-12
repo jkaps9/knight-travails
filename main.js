783fa0f4f3a072bea2a377d4bbffdf89d29f6a02
Josh <15219491+jkaps9@users.noreply.github.com>
2025-06-11 21:40:31 -0400

    Set up structure

---
+++ main.js
@@ -0,0 +1,11 @@
+// https://www.theodinproject.com/lessons/javascript-knights-travails
+
+function knightMoves(start, end) {
+  // shows the shortest possible way to get from one square to another by outputting all squares the knight will stop on along the way.
+  // You can think of the board as having 2-dimensional coordinates. Calling your function would therefore look like:
+  //knightMoves([0,0],[1,2]) // returns [[0,0],[1,2]]
+}
+
+function getLegalMoves(start) {
+  //return all legal moves from the starting position represented as [x,y]
+}
