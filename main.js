9046ca7378595365db0745636993a778c73c3259
Josh <15219491+jkaps9@users.noreply.github.com>
2025-06-11 21:48:09 -0400

    Build function to return legal moves from start

--- main.js
+++ main.js
@@ -4,8 +4,32 @@ function knightMoves(start, end) {
   // shows the shortest possible way to get from one square to another by outputting all squares the knight will stop on along the way.
   // You can think of the board as having 2-dimensional coordinates. Calling your function would therefore look like:
   //knightMoves([0,0],[1,2]) // returns [[0,0],[1,2]]
+
+  const arr = getLegalMoves(start);
+  console.log(arr);
 }
 
 function getLegalMoves(start) {
   //return all legal moves from the starting position represented as [x,y]
+  //Its basic move is two steps forward and one step to the side or one step forward and two steps to the side. It can face any direction.
+  const moves = [];
+
+  const x = start[0];
+  const y = start[1];
+
+  if (x < 6 && y < 5) moves.push([x + 1, y + 2]);
+  if (x > 0 && y < 5) moves.push([x - 1, y + 2]);
+
+  if (x < 6 && y > 1) moves.push([x + 1, y - 2]);
+  if (x > 0 && y > 1) moves.push([x - 1, y - 2]);
+
+  if (x < 5 && y < 6) moves.push([x + 2, y + 1]);
+  if (x > 1 && y < 6) moves.push([x - 2, y + 1]);
+
+  if (x < 5 && y > 0) moves.push([x + 2, y - 1]);
+  if (x > 1 && y > 0) moves.push([x - 2, y - 1]);
+
+  return moves;
 }
+
+knightMoves([3, 3], [0, 0]);
